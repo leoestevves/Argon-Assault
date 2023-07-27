d601e76ecfa0d1f102ff03bb17a4621bd1554c47
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-07-27 15:17:02 -0300

    Player movement
    Making player movement

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 25f;
+    [SerializeField] float xRange = 9f;
+    [SerializeField] float yMinRange = -3f;
+    [SerializeField] float yMaxRange = 10.5f;
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
 
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        float xOffSet = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffSet = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, yMinRange, yMaxRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
