1578f701246d3c41ba6f4ab6f6c97167a1b82f89
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-07-31 14:35:28 -0300

    Making ship rotations
    Making x, y, z rotations, using Quaternion

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float controlSpeed = 25f;
     [SerializeField] float xRange = 9f;
     [SerializeField] float yMinRange = -3f;
     [SerializeField] float yMaxRange = 10.5f;
 
+    [Header("Rotation")]
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 1f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    [SerializeField] float rotationFactor = 1f;
+    
+    float xThrow, yThrow;
+
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {       
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        //transform.localRotation = Quaternion.Euler(pitch, yaw, roll); //Forma mais efetiva de fazer a rotação
+
+        //Deixa a rotação mais suave
+        Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, rotationFactor);
+
+
+
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffSet = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffSet;
