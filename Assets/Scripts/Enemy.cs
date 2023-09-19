3eb72eae4659cabb7357c050258af61f00ddecc5
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-09-19 14:37:08 -0300

    Score board logic
    Making the logic of score board.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,32 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
 
     void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); //Instanciando a explosão no local que o inimigo morre
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+    
 }
